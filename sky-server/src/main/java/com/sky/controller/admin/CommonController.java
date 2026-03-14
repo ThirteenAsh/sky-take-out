fd7427f2886f14a1a3a7024649978b81ff7914a6
ThirteenAsh <1984885454@qq.com>
2026-03-14 20:19:15 +0800

    feat(dish): 添加菜品管理和文件上传功能
    - 新增 CommonController 实现文件上传接口
    - 新增 DishController 实现菜品添加功能
    - 创建 DishFlavorMapper 和对应 XML 映射文件
    - 扩展 DishMapper 接口并添加菜品插入方法
    - 实现 DishService 接口及 DishServiceImpl 业务逻辑
    - 添加事务支持确保菜品和口味数据一致性
    - 集成 MyBatis 自动填充功能处理菜品基础字段

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,27 @@
+package com.sky.controller.admin;
+
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+public class CommonController {
+
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file){
+        log.info("文件上传");
+        String fillPath = "https://www.bytecraft.xin/cai.png";
+        return Result.success(fillPath);
+    }
+
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,49 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    @Transactional
+    @Override
+    public void addDish(@RequestBody DishDTO dishDTO) {
+
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+
+        //插入菜品
+        dishMapper.insert(dish);
+
+        //获取insert语句生成的主键值
+        Long id = dish.getId();
+
+        //插入口味
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors != null && !flavors.isEmpty()){
+            for (DishFlavor flavor : flavors) {
+                flavor.setDishId(id);
+            }
+            dishFlavorMapper.insertBatch(flavors);
+        }
+
+    }
+}
